b5fd103527617b12569335288826b009c2727e3b
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-03 08:12:03 -0300

    Add remove by id controller.

---
+++ src/Lumen/CreateRemoveController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tots\Installer\Lumen;
+
+use Illuminate\Support\Facades\DB;
+
+class CreateRemoveController extends CreateBaseController
+{
+    /**
+     * Path del archivo a tener de base
+     * @var string
+     */
+    protected $filePath = __DIR__ . '/../../data/lumen/create_remove_controller.txt';
+    /**
+     * Path de la carpeta donde se va a guardar
+     * @var string
+     */
+    protected $savePath = __DIR__ . '/../../../../../app/Http/Controllers/';
+
+    public function run()
+    {
+        $this->file = str_replace('%%nameClass%%', $this->getCamelCase($this->name), $this->file);
+        $this->file = str_replace('%%name%%', $this->name, $this->file);
+        
+        try {
+            mkdir($this->savePath . '/' . $this->getCamelCase($this->name), 0777, true);
+        } catch (\Exception $exc) { }
+        file_put_contents($this->savePath . '/' . $this->getCamelCase($this->name) . '/RemoveByIdController.php', $this->file);
+
+        // Agregamos route
+        $this->addRoute('delete', $this->name . 's/{id}', $this->getCamelCase($this->name) . '\\RemoveByIdController');
+    }
+}
