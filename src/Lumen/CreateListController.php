48471ad39800f5ea8d14f139ca89e9a336f02669
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-03 07:58:25 -0300

    Add create list controller.

--- src/Lumen/CreateListController.php
+++ src/Lumen/CreateListController.php
@@ -4,13 +4,13 @@ namespace Tots\Installer\Lumen;
 
 use Illuminate\Support\Facades\DB;
 
-class CreateFetchController extends CreateBaseController
+class CreateListController extends CreateBaseController
 {
     /**
      * Path del archivo a tener de base
      * @var string
      */
-    protected $filePath = __DIR__ . '/../../data/lumen/create_fetchbyid_controller.txt';
+    protected $filePath = __DIR__ . '/../../data/lumen/create_list_controller.txt';
     /**
      * Path de la carpeta donde se va a guardar
      * @var string
@@ -25,9 +25,9 @@ class CreateFetchController extends CreateBaseController
         try {
             mkdir($this->savePath . '/' . $this->getCamelCase($this->name), 0777, true);
         } catch (\Exception $exc) { }
-        file_put_contents($this->savePath . '/' . $this->getCamelCase($this->name) . '/FetchByIdController.php', $this->file);
+        file_put_contents($this->savePath . '/' . $this->getCamelCase($this->name) . '/ListController.php', $this->file);
 
         // Agregamos route
-        $this->addRoute('get', $this->name . 's/{id}', $this->getCamelCase($this->name) . '\\FetchByIdController');
+        $this->addRoute('get', $this->name . 's/', $this->getCamelCase($this->name) . '\\ListController');
     }
 }
